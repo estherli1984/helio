f6c1d1ab4effe1cc42120e1136c06dffb7b0b493
Roman Gershman <romange@gmail.com>
2023-03-03 08:49:15 +0200

    Add S3ReadFile
    Signed-off-by: Roman Gershman <romange@gmail.com>

--- util/cloud/s3_file.cc
+++ util/cloud/s3_file.cc
@@ -3,11 +3,11 @@
 //
 #include "util/cloud/s3_file.h"
 
-
-#include <boost/beast/http/dynamic_body.hpp>
-#include <boost/beast/http/buffer_body.hpp>
-
 #include <absl/strings/str_cat.h>
+
+#include <boost/beast/http/buffer_body.hpp>
+#include <boost/beast/http/dynamic_body.hpp>
+
 #include "base/logging.h"
 #include "util/cloud/cloud_utils.h"
 
@@ -42,11 +42,8 @@ std::ostream& operator<<(std::ostream& os, const h2::response<h2::buffer_body>& 
   return os;
 }
 
-
 class S3ReadFile : public io::ReadonlyFile {
  public:
-
-
   // does not own pool object, only wraps it with ReadonlyFile interface.
   S3ReadFile(AWS* aws, http::Client* client, string read_obj_url)
       : aws_(*aws), client_(client), read_obj_url_(std::move(read_obj_url)) {
@@ -86,6 +83,10 @@ class S3ReadFile : public io::ReadonlyFile {
   size_t size_ = 0, offs_ = 0;
 };
 
+S3ReadFile::~S3ReadFile() {
+  Close();
+}
+
 std::error_code S3ReadFile::Open() {
   string url = absl::StrCat("/", read_obj_url_);
   h2::request<h2::empty_body> req{h2::verb::get, url, 11};
@@ -104,7 +105,8 @@ std::error_code S3ReadFile::Open() {
   CHECK(parser_.keep_alive()) << "TBD";
   const auto& msg = parser_.get();
 
-  VLOG(1) << "HeaderResp(" << "): " << msg;
+  VLOG(1) << "HeaderResp("
+          << "): " << msg;
 
   auto content_len_it = msg.find(h2::field::content_length);
   if (content_len_it != msg.end()) {
@@ -119,6 +121,66 @@ std::error_code S3ReadFile::Open() {
   }
 
   return ec;
+}
+
+error_code S3ReadFile::Close() {
+  return error_code{};
+}
+
+io::Result<size_t> S3ReadFile::Read(size_t offset, const iovec* v, uint32_t len) {
+  if (offset != offs_) {
+    return nonstd::make_unexpected(make_error_code(errc::invalid_argument));
+  }
+
+  // We can not cache parser() into local var because Open() below recreates the parser instance.
+  if (parser_.is_done()) {
+    return 0;
+  }
+
+  size_t index = 0;
+  size_t read_sofar = 0;
+
+  while (index < len) {
+    // We keep body references inside the loop because Open() that might be called here,
+    // will recreate the parser from the point the connections disconnected.
+    auto& body = parser()->get().body();
+    auto& left_available = body.size;
+    body.data = v[index].iov_base;
+    left_available = v[index].iov_len;
+
+    boost::system::error_code ec = client_->Recv(parser());  // decreases left_available.
+    size_t http_read = v[index].iov_len - left_available;
+
+    if (!ec || ec == h2::error::need_buffer) {  // Success
+      DVLOG(2) << "Read " << http_read << " bytes from " << offset << " with capacity "
+               << v[index].iov_len << "ec: " << ec;
+
+      CHECK(left_available == 0 || !ec);
+
+      // This check does not happen. See here why: https://github.com/boostorg/beast/issues/1662
+      // DCHECK_EQ(sz_read, http_read) << " " << range.size() << "/" << left_available;
+      offs_ += http_read;
+      read_sofar += http_read;
+      ++index;
+
+      continue;
+    }
+
+    if (ec == h2::error::partial_message) {
+      offs_ += http_read;
+      VLOG(1) << "Got partial_message";
+
+      // advance the destination buffer as well.
+      read_sofar += http_read;
+      break;
+    }
+
+
+    LOG(ERROR) << "ec: " << ec << "/" << ec.message() << " at " << offset << "/" << size_;
+    return nonstd::make_unexpected(ec);
+  }
+
+  return read_sofar;
 }
 
 }  // namespace
